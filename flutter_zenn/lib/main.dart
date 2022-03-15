0bc31db1ca7bbc1f474f43aefe35ea768ffe5637
yugo-miyabe <yuni42yuyuyu@gmail.com>
2022-03-15 15:35:44 +0900

    ドロワーメニューを作成

--- flutter_zenn/lib/main.dart
+++ flutter_zenn/lib/main.dart
@@ -13,38 +13,21 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-          appBar: AppBar(title: Text("Flutter Demo")),
-          body: Column(
+        appBar: AppBar(
+          title: Row(
             children: [
-              Text("初めてのテキスト"),
-              Text("2番目ののテキスト"),
-              TextButton(
-                child: Text("更新"),
-                onPressed: () => {print("ボタン押下")},
-              ),
-              Row(
-                //spaceAround...並べた子の間に空きスペースを均等に配置する。
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Icon(
-                    Icons.favorite,
-                    color: Colors.pink,
-                    size: 24.0,
-                  ),
-                  Icon(
-                    Icons.audiotrack,
-                    color: Colors.green,
-                    size: 30.0,
-                  ),
-                  Icon(
-                    Icons.beach_access,
-                    color: Colors.blue,
-                    size: 36.0,
-                  )
-                ],
-              ),
+              Icon(Icons.create),
+              Text("初めてのタイトル"),
             ],
-          )),
+          ),
+        ),
+        drawer: Drawer(
+          child: Center(
+            child: Text("Drawer"),
+          ),
+        ),
+        body: Text("初めてのテキスト"),
+      ),
     );
   }
 }
