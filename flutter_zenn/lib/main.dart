d4c7ac6be7f82dfe045217e093f2ac95013d8217
yugo-miyabe <yuni42yuyuyu@gmail.com>
2022-03-15 15:50:59 +0900

    テキストを動的に変更する処理を追加

--- flutter_zenn/lib/main.dart
+++ flutter_zenn/lib/main.dart
@@ -12,25 +12,66 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              Icon(Icons.create),
-              Text("初めてのタイトル"),
-            ],
-          ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _type = "偶数";
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+    if (_counter % 2 == 0) {
+      _type = "偶数";
+    } else {
+      _type = "奇数";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          //
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              '$_type',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.red,
+              ),
+            )
+          ],
         ),
-        drawer: Drawer(
-          child: Center(
-            child: Text("Drawer"),
-          ),
-        ),
-        body: Text("初めてのテキスト"),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {print("押下")},
-          child: Icon(Icons.timer),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
