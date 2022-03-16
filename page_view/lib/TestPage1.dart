4a5225c8f809f85b3fffeeb963e8808670d32ebf
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-16 15:30:14 +0900

    クラス追加

---
+++ page_view/lib/TestPage1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TestPage1 extends StatelessWidget {
+  const TestPage1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
