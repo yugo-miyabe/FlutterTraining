3f95a19408a96a9a012c7f49bc114fc4f1e54f4b
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-16 17:17:42 +0900

    非同期メソッド作成

--- asynchronous_processing/lib/Async.dart
+++ asynchronous_processing/lib/Async.dart
@@ -1,20 +1,19 @@
-import 'dart:io';
-
 class Async {
   void asynctest1() {
     print("method begin");
-    print(DateTime.now().toString());
+    print("now time " + DateTime.now().toString());
     print("data1 start");
-    print(syncFunc("data1", 3));
+    print(asyncFunc("data1", 3));
     print("data2 start");
-    print(syncFunc("data2", 2));
+    print(asyncFunc("data2", 2));
     print("data3 start");
-    print(syncFunc("data3", 1));
+    print(asyncFunc("data3", 1));
   }
 
   // timeの時間分スリープし、その後現在時間を返す関数
-  String syncFunc(String name, int time) {
-    sleep(Duration(seconds: time));
-    return name + ":" + DateTime.now().toString();
+  Future<String> asyncFunc(String name, int time) async {
+    return Future.delayed(Duration(seconds: time), () {
+      return name + ":" + DateTime.now().toString();
+    });
   }
 }
