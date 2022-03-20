2d0c4a8e7b3bcaf95197ec4404eac4c4e6ee6324
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-20 12:49:05 +0900

    コメント追加

--- git_application/lib/model/git_user.dart
+++ git_application/lib/model/git_user.dart
@@ -1,12 +1,17 @@
 class GitUser {
+  /// ログインID
   final String login;
+
+  /// アバターURL
   final String avatorUrl;
 
+  /// コンストラクタ
   const GitUser({
     required this.login,
     required this.avatorUrl,
   });
 
+  /// ファクトリコンストラクタ
   factory GitUser.fromJson(Map<String, dynamic> json) {
     return GitUser(
       login: json['login'],
