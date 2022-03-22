74b900c49214ddb43e9f7411dfdd2f33a9482bba
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-22 14:05:52 +0900

    余分なダブルクオーテーション削除

--- git_application/lib/repository/repository_request.dart
+++ git_application/lib/repository/repository_request.dart
@@ -4,8 +4,8 @@ import 'package:git_application/model/repository.dart';
 import 'package:http/http.dart' as http;
 
 Future<List<Repository>> fetchRepository(String userName) async {
-  http.Response response = await http
-      .get(Uri.parse('https://api.github.com/users/$userName/repos"'));
+  http.Response response =
+      await http.get(Uri.parse('https://api.github.com/users/$userName/repos'));
 
   if (response.statusCode == 200) {
     final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
