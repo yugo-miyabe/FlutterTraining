ef22b17baac1c94df2123e7a6736cb8cb7d65be5
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-23 15:02:48 +0900

    ホーム画面作成

---
+++ admin_mobile/lib/side_navigation.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SideNavigation extends StatefulWidget {
+  const SideNavigation({Key? key}) : super(key: key);
+
+  @override
+  _SideNavigationState createState() => _SideNavigationState();
+}
+
+class _SideNavigationState extends State<SideNavigation> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (index) {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      destinations: const [
+        NavigationRailDestination(
+          icon: Icon(Icons.thumbs_up_down),
+          label: Text('ThumbsUpDown'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.people),
+          label: Text('people'),
+        )
+      ],
+    );
+  }
+}
