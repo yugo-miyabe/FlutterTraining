9a51b4f4681665e617fa6dd112b75a1d6bb78a8a
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-24 14:09:50 +0900

    コメント追加

--- admin_mobile/lib/view/post.dart
+++ admin_mobile/lib/view/post.dart
@@ -36,7 +36,10 @@ class Post extends StatelessWidget {
         ),
         child: Column(
           children: [
+            // タイトル部分
             ListTile(
+              // leading...タイトルの前に表示するwidget
+              // ClipOval...楕円形のwidget
               leading: ClipOval(
                 child: Container(
                   color: colorPrimary,
@@ -44,6 +47,7 @@ class Post extends StatelessWidget {
                   height: 48,
                   child: Center(
                     child: Text(
+                      // 先頭から1文字目を表示
                       name.substring(0, 1),
                       style: const TextStyle(color: Colors.white, fontSize: 24),
                     ),
@@ -53,41 +57,56 @@ class Post extends StatelessWidget {
               title: Text(name),
               subtitle: const Text('2 min ago'),
             ),
+            // 説明部分の描画
             Container(
+              // 左右のマージンを設定
               padding: const EdgeInsets.symmetric(horizontal: 8),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  //　余白を配置
                   const SizedBox(width: 72),
                   Container(
                     width: 16,
                     height: 16,
+                    // ボックスを描画
                     decoration: BoxDecoration(
+                      // 枠線を指定
                       border: Border.all(color: colorPrimary, width: 4),
+                      // 角の度合い
                       borderRadius: BorderRadius.circular(8),
                     ),
                   ),
+                  // 余白を配置
                   const SizedBox(width: 8),
+                  // 余白に対して最大幅で表示
                   Flexible(child: Text(message)),
                 ],
               ),
             ),
+            // リンクの描画
             Container(
+              // 内側の余白の設定
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              // 行表示
               child: Row(
                 children: [
                   Container(
+                    // アンダーライン
                     decoration: BoxDecoration(
                       border: Border(
                         bottom: BorderSide(color: colorPrimary, width: 2),
                       ),
                     ),
+                    // テキストの表示
                     child: Text(
                       textReason,
                       style: const TextStyle(color: Colors.blueAccent),
                     ),
                   ),
+                  //　余白の設定
                   const SizedBox(width: 24),
+                  // 余白いっぱいに表示
                   Expanded(
                     child: TextButton(
                       style: TextButton.styleFrom(
@@ -97,11 +116,14 @@ class Post extends StatelessWidget {
                       child: Text(textNegative),
                     ),
                   ),
+                  //　余白の設定
                   const SizedBox(width: 8),
+                  // 余白いっぱいに表示
                   Expanded(
                     child: TextButton(
                       style: TextButton.styleFrom(
                         primary: colorPositive,
+                         // withOpacity.. 半透明
                         backgroundColor: colorPositive.withOpacity(0.2),
                       ),
                       onPressed: () {},
