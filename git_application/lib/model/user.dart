94076b312f6e9f80839c982398ad841f462c7bfe
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-25 19:10:47 +0900

    不要なクラスを削除

--- git_application/lib/model/user.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part '../freezed/user.freezed.dart';
-
-@freezed
-class User with _$User {
-  const factory User(String login, String avatorUrl) = _User;
-}
