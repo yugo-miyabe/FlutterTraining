a28a59bffd6382fd503a1efaaab0cf6c1abff23c
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-25 17:32:48 +0900

    関数作成

--- freezed/lib/main.dart
+++ freezed/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:freezed_study/user.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,6 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _counter++;
     });
+    func();
   }
 
   @override
@@ -63,5 +65,35 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void func() {
+    // ユーザを3つ作成する。user1とuser3は同じ内容
+    User user1 = User('kazutxt', 30);
+    User user2 = User('kazutxt2', 32);
+    User user3 = User('kazutxt', 30);
+
+    // 表示(toString)
+    print("check1");
+    print(user1);
+
+    // 比較(==)
+    print("check2");
+    if (user1 == user2) print("user1とuser2は同じ人");
+    if (user1 == user3) print("user1とuser3は同じ人");
+
+    // コピーをして新しいインスタンスを作成(user1は変わらない)
+    print("check3");
+    User user4 = user1.copyWith(name: "unknown");
+    print(user1);
+    print(user4);
+
+    // 参照そのものを変えるのはOK
+    print("check4");
+    user2 = user3;
+    print(user2);
+
+    // immutableを破壊するので、以下のような使い方はNG
+    // user1.name = "unknown";
   }
 }
