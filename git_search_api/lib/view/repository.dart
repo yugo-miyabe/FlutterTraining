2f804d82ded20be3c086534401c3ac9c919ed66f
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-28 17:47:35 +0900

    テキスト入力できるよう変更

--- git_search_api/lib/view/repository.dart
+++ git_search_api/lib/view/repository.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 
+class Repository extends StatefulWidget {
+  const Repository({Key? key}) : super(key: key);
+
+  @override
+  State<Repository> createState() => _RepositoryState();
+}
+
+class _RepositoryState extends State<Repository> {
+  @override
+  Widget build(BuildContext context) {
+    String? searchText;
+
+    return Container(
+      padding: const EdgeInsets.all(60),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+            onChanged: (String search) {
+              searchText = search;
+            },
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              print(searchText);
+            },
+            child: const Text('Search'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+/*
 class Repository extends StatelessWidget {
   const Repository({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    String searchText;
+
     return Container(
       padding: const EdgeInsets.all(60),
       child: Column(
@@ -14,7 +55,10 @@ class Repository extends StatelessWidget {
             decoration: InputDecoration(
               border: OutlineInputBorder(),
             ),
+            onChanged: (text){
+            },
           ),
+
           const SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {},
@@ -25,3 +69,5 @@ class Repository extends StatelessWidget {
     );
   }
 }
+
+ */
