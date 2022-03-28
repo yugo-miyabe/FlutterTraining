65961275ba7135f6590e681387e9e2725d6ed624
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-28 17:29:45 +0900

    パッケージ変更

--- git_search_api/test/widget_test.dart
+++ git_search_api/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:git_search_api/main.dart';
+import 'package:git_search_api/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
