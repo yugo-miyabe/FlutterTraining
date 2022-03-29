2eb0961d423ce3f42545c7632246b289ea029d3e
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-29 17:17:36 +0900

    タイトルを変更する処理追加

--- git_search_api/lib/view/screen/my_home_page.dart
+++ git_search_api/lib/view/screen/my_home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:git_search_api/view/widget/repository_widget.dart';
 import 'package:git_search_api/view/widget/favorites_widget.dart';
+import 'package:git_search_api/view/widget/repository_widget.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -17,6 +17,10 @@ class _MyHomePageState extends State<MyHomePage> {
     Repository(),
     Favorites(),
   ];
+  final _title = <String>[
+    'Search',
+    'Favorites',
+  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -29,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Search'),
+        title: Text(_title[_selectedIndex]),
       ),
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
