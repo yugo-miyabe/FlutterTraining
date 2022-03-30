3c656b1146eb6d977fad60f09fc20b726a4c49c8
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-03-30 11:05:09 +0900

    テキストフィールドにデコレーション追加

--- git_search_api/lib/view/widget/repository_widget.dart
+++ git_search_api/lib/view/widget/repository_widget.dart
@@ -1,43 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:git_search_api/view/screen/repository_list.dart';
 
-class RepositoryWidget extends StatelessWidget {
+class RepositoryWidget extends StatefulWidget {
   const RepositoryWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    String? searchText;
+  State<RepositoryWidget> createState() => _RepositoryWidgetState();
+}
 
+class _RepositoryWidgetState extends State<RepositoryWidget> {
+  String searchText = "";
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(60),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TextField(
+            maxLength: 15,
             decoration: const InputDecoration(
               border: OutlineInputBorder(),
             ),
-            onChanged: (String search) {
-              searchText = search;
+            onChanged: (String text) {
+              setState(() {
+                searchText = text;
+              });
             },
           ),
-          const SizedBox(height: 20),
+          const SizedBox(height: 30),
           ElevatedButton(
-            onPressed: () {
-              // TODO nullの場合入力エラーを出す
-              // TODO 共通化
-              print(searchText);
-              if (searchText != null) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => RepositoryList(searchText!),
-                  ),
-                );
-              }
-            },
             child: const Text('Search'),
-          )
+            onPressed: searchText.isNotEmpty
+                ? () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RepositoryList(searchText!),
+                      ),
+                    );
+                  }
+                : null,
+          ),
         ],
       ),
     );
